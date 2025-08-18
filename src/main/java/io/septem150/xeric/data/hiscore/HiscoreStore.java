ac9c2223c4371358f91874d2f64287a0c4897a2d
Carson Mullins <carsonmullins@yahoo.com>
2025-08-17 19:42:12 -0500

    chore: clean up release for plugin-hub

--- src/main/java/io/septem150/xeric/data/hiscore/HiscoreStore.java
+++
@@ -1,8 +0,0 @@
-package io.septem150.xeric.data.hiscore;
-
-import java.util.List;
-import lombok.NonNull;
-
-public interface HiscoreStore {
-  @NonNull List<Hiscore> getAll();
-}
--- src/main/java/io/septem150/xeric/panel/leaderboard/LeaderboardPanel.java
+++ src/main/java/io/septem150/xeric/panel/leaderboard/LeaderboardPanel.java
@@ -1,18 +1,19 @@
 package io.septem150.xeric.panel.leaderboard;
 
-import io.septem150.xeric.data.hiscore.HiscoreStore;
-import io.septem150.xeric.data.player.ClanRank;
-import java.awt.BorderLayout;
-import java.util.AbstractCollection;
+import io.septem150.xeric.util.ResourceUtil;
+import java.awt.Component;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.client.callback.ClientThread;
-import net.runelite.client.game.SpriteManager;
 import net.runelite.client.ui.ColorScheme;
+import net.runelite.client.ui.FontManager;
+import net.runelite.client.util.LinkBrowser;
 
 @Slf4j
 @Singleton
@@ -20,52 +21,40 @@ public class LeaderboardPanel extends JPanel {
   public static final String TOOLTIP = "Leaderboard";
   public static final String TAB_ICON = "/skill_icons/overall.png";
 
-  private final HiscoresTableModel hiscoresTableModel;
-  private final HiscoreStore hiscoreStore;
+  private final BoxLayout layout = new BoxLayout(this, BoxLayout.Y_AXIS);
+  private final JLabel wipLabel = new JLabel();
+  private final JButton womButton = new JButton();
 
   @Inject
-  private LeaderboardPanel(
-      HiscoreStore hiscoreStore, ClientThread clientThread, SpriteManager spriteManager) {
-    this.hiscoreStore = hiscoreStore;
-    setLayout(new BorderLayout());
+  private LeaderboardPanel() {
+    makeLayout();
+    makeStaticData();
+  }
+
+  private void makeLayout() {
+    removeAll();
+    setLayout(layout);
+    wipLabel.setBorder(new EmptyBorder(20, 0, 20, 0));
+    wipLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+    add(wipLabel);
+    womButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+    add(womButton);
+  }
+
+  private void makeStaticData() {
     setBackground(ColorScheme.DARK_GRAY_COLOR);
-    hiscoresTableModel = new HiscoresTableModel();
-    final JTable hiscoresTable = new JTable(hiscoresTableModel);
-    hiscoresTable.setCellSelectionEnabled(false);
-    hiscoresTable.setColumnSelectionAllowed(false);
-    hiscoresTable.setRowSelectionAllowed(false);
-    hiscoresTable.setDefaultRenderer(
-        AbstractCollection.class, new ExceptionsRenderer(clientThread, spriteManager));
-    hiscoresTable.setDefaultRenderer(ClanRank.class, new RankRenderer(clientThread, spriteManager));
-    hiscoresTable.setDefaultRenderer(
-        Username.class, new UsernameRenderer(clientThread, spriteManager));
-    hiscoresTable.setFillsViewportHeight(true);
-    hiscoresTable.getColumnModel().getColumn(0).setPreferredWidth(38);
-    hiscoresTable.getColumnModel().getColumn(1).setPreferredWidth(96);
-    hiscoresTable.getColumnModel().getColumn(2).setPreferredWidth(38);
-    hiscoresTable.getColumnModel().getColumn(3).setPreferredWidth(40);
-
-    JScrollPane scrollPane = new JScrollPane(hiscoresTable);
-    scrollPane.setWheelScrollingEnabled(true);
-    add(scrollPane, BorderLayout.CENTER);
+    wipLabel.setText(
+        "<html><body style='text-align: center'>Leaderboard coming soon!<br>Check out the Wise Old"
+            + " Man group in the meantime.</body></html>");
+    wipLabel.setFont(FontManager.getRunescapeSmallFont());
+    wipLabel.setForeground(ColorScheme.BRAND_ORANGE);
+    womButton.setIcon(new ImageIcon(ResourceUtil.getImage("wiseoldman_icon.png")));
+    womButton.addActionListener(
+        event -> LinkBrowser.browse("https://wiseoldman.net/groups/1200/hiscores"));
+    womButton.setToolTipText("View the Leaderboard on Wise Old Man");
   }
 
-  public void startUp() {
-    hiscoresTableModel.setData(
-        hiscoreStore.getAll().stream()
-            .map(
-                hiscore ->
-                    new Object[] {
-                      ClanRank.fromPoints(hiscore.getPoints()),
-                      Username.builder()
-                          .username(hiscore.getUsername())
-                          .accountType(hiscore.getAccountType())
-                          .build(),
-                      hiscore.getAccountExceptions(),
-                      hiscore.getPoints()
-                    })
-            .toArray(Object[][]::new));
-  }
+  public void startUp() {}
 
   public void refresh() {
     revalidate();
