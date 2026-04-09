2e3da09ef506b94ab647622190acf3f777080855
Carly Mullins <carson.mullins@proton.me>
2026-04-08 19:54:28 -0500

    add leaderboard panel

--- src/main/java/io/septem150/xeric/panel/leaderboard/LeaderboardPanel.java
+++ src/main/java/io/septem150/xeric/panel/leaderboard/LeaderboardPanel.java
@@ -4,15 +4,14 @@ import io.septem150.xeric.util.ResourceUtil;
 import java.awt.Component;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.client.ui.ColorScheme;
-import net.runelite.client.ui.FontManager;
 import net.runelite.client.util.LinkBrowser;
 
 @Slf4j
@@ -22,7 +21,7 @@ public class LeaderboardPanel extends JPanel {
   public static final String TAB_ICON = "/skill_icons/overall.png";
 
   private final BoxLayout layout = new BoxLayout(this, BoxLayout.Y_AXIS);
-  private final JLabel wipLabel = new JLabel();
+  private final JButton leaderboardButton = new JButton();
   private final JButton womButton = new JButton();
 
   @Inject
@@ -34,24 +33,25 @@ public class LeaderboardPanel extends JPanel {
   private void makeLayout() {
     removeAll();
     setLayout(layout);
-    wipLabel.setBorder(new EmptyBorder(20, 0, 20, 0));
-    wipLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-    add(wipLabel);
+    setBorder(new EmptyBorder(0, 20, 0, 20));
+    leaderboardButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+    add(leaderboardButton);
+    add(Box.createVerticalStrut(5));
     womButton.setAlignmentX(Component.CENTER_ALIGNMENT);
     add(womButton);
   }
 
   private void makeStaticData() {
     setBackground(ColorScheme.DARK_GRAY_COLOR);
-    wipLabel.setText(
-        "<html><body style='text-align: center'>Leaderboard coming soon!<br>Check out the Wise Old"
-            + " Man group in the meantime.</body></html>");
-    wipLabel.setFont(FontManager.getRunescapeSmallFont());
-    wipLabel.setForeground(ColorScheme.BRAND_ORANGE);
-    womButton.setIcon(new ImageIcon(ResourceUtil.getImage("wiseoldman_icon.png")));
+    leaderboardButton.setIcon(new ImageIcon(ResourceUtil.getImage("sidepanel_icon.png", 32, 32)));
+    leaderboardButton.setText("Clan Leaderboard");
+    leaderboardButton.addActionListener(event -> LinkBrowser.browse("https://projectxeric.com"));
+    leaderboardButton.setToolTipText("View the Zeah Ironmen Leaderboard");
+    womButton.setIcon(new ImageIcon(ResourceUtil.getImage("wiseoldman_icon.png", 32, 32)));
+    womButton.setText("Wise Old Man");
     womButton.addActionListener(
         event -> LinkBrowser.browse("https://wiseoldman.net/groups/1200/hiscores"));
-    womButton.setToolTipText("View the Leaderboard on Wise Old Man");
+    womButton.setToolTipText("View the Wise Old Man Group");
   }
 
   public void refresh() {
